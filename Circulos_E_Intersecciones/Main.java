ec32883dfdb635d9d4978685d2c10cacb50a8da8
mati-otero <77358227+mati-otero@users.noreply.github.com>
2021-07-28 20:31:18 -0300

    Create Main.java

---
+++ Circulos_E_Intersecciones/Main.java
@@ -0,0 +1,12 @@
+package circuloeintersecciones;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Circulo c1 = new Circulo(new Punto(0, 0), 1);
+		Circulo c2 = new Circulo(new Punto(1, 1), 1.5);
+		System.out.println(c1.intersectaCon(c2) == true);
+
+	}
+}
